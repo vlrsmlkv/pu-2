dca1913f3e84ac321993bf4728c04f76dc331d2e
vlrsmlkv <vlrsmlkv@gmail.com>
2021-07-04 00:44:17 +0300

    fix: fix disabled checkbox

--- src/components/CheckBox.jsx
+++ src/components/CheckBox.jsx
@@ -7,7 +7,7 @@ const CheckBox = ({onChange}) => {
 
     return (
         <div className ="checkbox-container">
-            <input type="checkbox" className="checkbox" onChange={onChangeHandler}/>
+            <input type="checkbox" id="checkbox" className="checkbox" onChange={onChangeHandler}/>
             <label htmlFor="checkbox" className="checkbox-label">Отображать каждый файл на отдельном листе</label> 
         </div>
     )
