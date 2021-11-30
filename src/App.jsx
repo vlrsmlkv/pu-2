4f345e9ae640abe266cdccec388a27cfd99a0431
vlrsmlkv <vlrsmlkv@gmail.com>
2021-12-01 01:42:36 +0300

    11: fix router

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import Main from "./pages/Main";
 import Info from "./pages/Info";
 import NavBar from "./components/NavBar";
+import NotFoundPage from "./components/NotFoundPage";
 
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 const App = () => (
-  
-    <Router basename="pu-2">
-        <NavBar/>
+  <Router>
+    <Route exact path="/">
+      <Redirect to={process.env.PUBLIC_URL} />
+    </Route> 
+    <Route>
+      <Router basename={process.env.PUBLIC_URL}>
+        <NavBar />
         <div className="App">
           <Switch>
             <Route exact path="/">
@@ -17,13 +22,12 @@ const App = () => (
             <Route exact path="/info">
               <Info />
             </Route>
-            <Route>
-              <Redirect to="/"/>
-            </Route>
+            <Route path="*" component={NotFoundPage} />
           </Switch>
         </div>
-    </Router>
-
+      </Router>
+    </Route>
+  </Router>
 );
 
 export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,36 +2,34 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { Disc, Info } from 'react-feather';
 
-const NavBar = () => {
-  return (
-    <div className="nav-bar">
-      <NavLink 
-        className="nav-bar-button"
-        exact
-        to="/" 
-        activeClassName="selected" 
-        activeStyle={{
-          fontWeight: "bold",
-          borderColor: "black"
-        }}
-      >
-        <Disc className="nav-bar-button-icon"/> Main
-      </NavLink>
+const NavBar = () => (
+  <div className="nav-bar">
+    <NavLink
+      className="nav-bar-button"
+      exact
+      to="/"
+      activeClassName="selected"
+      activeStyle={{
+        fontWeight: "bold",
+        borderColor: "black"
+      }}
+    >
+      <Disc className="nav-bar-button-icon"/> Main
+    </NavLink>
 
-      <NavLink 
-        className="nav-bar-button"
-        exact
-        to="/info" 
-        activeClassName="selected" 
-        activeStyle={{
-          fontWeight: "bold",
-          borderColor: "black"
-        }}
-      >
-        <Info className="nav-bar-button-icon"/> Info
-      </NavLink>
-    </div> 
-  );
-}
+    <NavLink
+      className="nav-bar-button"
+      exact
+      to="/info"
+      activeClassName="selected"
+      activeStyle={{
+        fontWeight: "bold",
+        borderColor: "black"
+      }}
+    >
+      <Info className="nav-bar-button-icon"/> Info
+    </NavLink>
+  </div> 
+);
 
 export default NavBar;
